65954a832d1f982a8964c016f8a78b903eac5734
Lorendacol <dacolloren@gmail.com>
2025-06-05 14:25:23 +0800

    the home for the system

---
+++ src/pages/Home.tsx
@@ -0,0 +1,77 @@
+import { 
+  IonButton,
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonIcon, 
+      IonLabel, 
+      IonMenuButton, 
+      IonPage, 
+      IonRouterOutlet, 
+      IonTabBar, 
+      IonTabButton, 
+      IonTabs,
+      IonTitle,
+
+      IonToolbar 
+
+} from '@ionic/react';
+
+import { IonReactRouter } from '@ionic/react-router';
+import { bookOutline, search, star } from 'ionicons/icons';
+import { Route, Redirect } from 'react-router';
+
+import Favorites from './home-tabs/Favorites';
+import Feed from './home-tabs/Feed';
+import Search from './home-tabs/Search';
+
+const Home: React.FC = () => {
+  const tabs = [
+    {name: 'Feed', tab: 'feed', url: '/it35-lab/app/home/feed', icon: bookOutline},
+    {name: 'Search', tab: 'search', url: '/it35-lab/app/home/search', icon: search},
+    {name: 'Favorites', tab: 'favorites', url: '/it35-lab/app/home/favorites', icon: star},
+  ];
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      
+      <IonContent>
+        <IonTabs>
+          <IonRouterOutlet>
+            <Route exact path="/it35-lab/app/home/feed">
+              <Feed />
+            </Route>
+            <Route exact path="/it35-lab/app/home/search">
+              <Search />
+            </Route>
+            <Route exact path="/it35-lab/app/home/favorites">
+              <Favorites />
+            </Route>
+            <Route exact path="/it35-lab/app/home">
+              <Redirect to="/it35-lab/app/home/feed" />
+            </Route>
+          </IonRouterOutlet>
+
+          <IonTabBar slot="bottom">
+            {tabs.map((item, index) => (
+              <IonTabButton key={index} tab={item.tab} href={item.url}>
+                <IonIcon icon={item.icon} />
+                <IonLabel>{item.name}</IonLabel>
+              </IonTabButton>
+            ))}
+          </IonTabBar>
+        </IonTabs>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
